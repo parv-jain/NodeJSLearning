77938ca5ed90f8f80dd7c79a1effa950f6b6620a
Parv Jain <parvjn616@gmail.com>
2017-08-23 21:57:35 +0530

    join documents(records) to display

---
+++ nodejs-mongodb/join.js
@@ -0,0 +1,43 @@
+/*
+@Author: Parv Jain
+Join Collections
+----------------
+MongoDB is not a relational database, but you can perform a left outer join by using the $lookup stage.
+
+The $lookup stage lets you specify which collection you want to join with the current collection, and which fields that should match.
+
+Consider you have a "orders" collection and a "products" collection
+
+orders
+[
+  { _id: 1, product_id: 154, status: 1 }
+]
+products
+[
+  { _id: 154, name: 'Chocolate Heaven' },
+  { _id: 155, name: 'Tasty Lemons' },
+  { _id: 156, name: 'Vanilla Dreams' }
+]
+
+*/
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://127.0.0.1:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  db.collection('orders').aggregate([
+    { $lookup:
+       {
+         from: 'products',
+         localField: 'product_id',
+         foreignField: 'id',
+         as: 'orderdetails'
+       }
+     }
+    ], function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    db.close();
+  });
+});
