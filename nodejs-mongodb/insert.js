5d71ebee417603d93cd3f0e0128d4da3c05ff7ed
Parv Jain <parvjn616@gmail.com>
2017-08-23 19:43:52 +0530

    insert documents(records) to collection

---
+++ nodejs-mongodb/insert.js
@@ -0,0 +1,64 @@
+/*
+@Author: Parv Jain
+
+Insert Into Collection
+----------------------
+To insert a record, or document as it is called in MongoDB, into a collection, we use the insertOne() method.
+
+A document in MongoDB is the same as a record in MySQL
+
+The first parameter of the insertOne() method is an object containing the name(s) and value(s) of each field in the document you want to insert.
+
+It also takes a callback function where you can work with any errors, or the result of the insertion
+*/
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/testdb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  db.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
+
+/*
+Insert Multiple Documents
+-------------------------
+To insert multiple documents into a collection in MongoDB, we use the insertMany() method.
+
+The first parameter of the insertMany() method is an array of objects, containing the data you want to insert.
+
+It also takes a callback function where you can work with any errors, or the result of the insertion
+*/
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var myobj = [
+    { name: 'John', address: 'Highway 71'},
+    { name: 'Peter', address: 'Lowstreet 4'},
+    { name: 'Amy', address: 'Apple st 652'},
+    { name: 'Hannah', address: 'Mountain 21'},
+    { name: 'Michael', address: 'Valley 345'},
+    { name: 'Sandy', address: 'Ocean blvd 2'},
+    { name: 'Betty', address: 'Green Grass 1'},
+    { name: 'Richard', address: 'Sky st 331'},
+    { name: 'Susan', address: 'One way 98'},
+    { name: 'Vicky', address: 'Yellow Garden 2'},
+    { name: 'Ben', address: 'Park Lane 38'},
+    { name: 'William', address: 'Central st 954'},
+    { name: 'Chuck', address: 'Main Road 989'},
+    { name: 'Viola', address: 'Sideway 1633'}
+  ];
+  db.collection("customers").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
+});
