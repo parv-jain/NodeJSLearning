52a840f60e858be4d525f53063845ca704ff6d01
Parv Jain <parvjn616@gmail.com>
2017-08-23 16:49:33 +0530

    added nodejs events

---
+++ nodeevents/eventdemo1.js
@@ -0,0 +1,14 @@
+/*
+
+Every action on a computer is an event. Like when a connection is made or a file is opened.
+
+Objects in Node.js can fire events, like the readStream object fires events when opening and closing a file
+
+*/
+
+var fs = require('fs');
+var rs = fs.createReadStream('./demofile.txt');
+rs.on('open', function () {
+  console.log('The file is open');
+});
+
