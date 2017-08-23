9d3188afd9241e6d89ab4c9f9bb4b487556b3387
Parv Jain <parvjn616@gmail.com>
2017-08-23 16:01:20 +0530

    initial commit

---
+++ demo_module.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var dt = require('./myfirstmodule');
+
+var server = http.createServer(function(req,res){
+				res.writeHead(200,{'Content-Type':'text/html'})
+				res.write("The date and time currently is:"+dt.myDateTime());
+				res.end();
+});
+
+server.listen(3000,console.log('server started at http://localhost:3000'));
